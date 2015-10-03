de2eb95827cac3549fddeb4804b595e4795282d6
Sanyam <sanyam.jain14@gmail.com>
2015-10-03 20:53:29 +0530

    checking db with title pk4

--- untitled1.py
+++ untitled1.py
@@ -105,20 +105,20 @@ def articles(article_id=None):
 
 @app.route("/update-db/", methods=["GET", "POST"])
 def upload():
-    toi_rss={'http://timesofindia.indiatimes.com/rssfeedstopstories.cms':'Top stories'}#,
-        # 'http://timesofindia.indiatimes.com/rssfeeds/1221656.cms':'Most Recent',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533916/index.rss':'India',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533917/index.rss':'World',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533919/index.rss':'Business',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533920/index.rss':'Cricket',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533921/index.rss':'Sports',
-        # 'http://dynamic.feedsportal.com/c/33039/f/533968/index.rss':'Health',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533922/index.rss':'Science',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533925/index.rss':'Environment',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533923/index.rss':'Technology',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533924/index.rss':'Education',
-        # 'http://timesofindia.feedsportal.com/c/33039/f/533928/index.rss':'Entertainment',
-        # 'http://timesofindia.indiatimes.com/rssfeeds/2886704.cms':'Lifestyle'}
+    toi_rss={'http://timesofindia.indiatimes.com/rssfeedstopstories.cms':'Top stories',
+        'http://timesofindia.indiatimes.com/rssfeeds/1221656.cms':'Most Recent',
+        'http://timesofindia.feedsportal.com/c/33039/f/533916/index.rss':'India',
+        'http://timesofindia.feedsportal.com/c/33039/f/533917/index.rss':'World',
+        'http://timesofindia.feedsportal.com/c/33039/f/533919/index.rss':'Business',
+        'http://timesofindia.feedsportal.com/c/33039/f/533920/index.rss':'Cricket',
+        'http://timesofindia.feedsportal.com/c/33039/f/533921/index.rss':'Sports',
+        'http://dynamic.feedsportal.com/c/33039/f/533968/index.rss':'Health',
+        'http://timesofindia.feedsportal.com/c/33039/f/533922/index.rss':'Science',
+        'http://timesofindia.feedsportal.com/c/33039/f/533925/index.rss':'Environment',
+        'http://timesofindia.feedsportal.com/c/33039/f/533923/index.rss':'Technology',
+        'http://timesofindia.feedsportal.com/c/33039/f/533924/index.rss':'Education',
+        'http://timesofindia.feedsportal.com/c/33039/f/533928/index.rss':'Entertainment',
+        'http://timesofindia.indiatimes.com/rssfeeds/2886704.cms':'Lifestyle'}
 
 
     for key, value in toi_rss.iteritems():
