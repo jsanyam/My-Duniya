9ef07966dc3bce2d13db12d19eab372e7221e1bf
Sanyam <sanyam.jain14@gmail.com>
2015-10-07 00:11:16 +0530

    db 2

--- untitled1.py
+++ untitled1.py
@@ -111,7 +111,7 @@ def articles(article_id=None):
 @app.route('/<category>', methods=["GET"])
 def tags(category):
     if request.method == 'GET':
-        tag = Article.query.filter(Article.category == category).limit(50)
+        tag = Article.query.filter(Article.category == category).order_by(Article.id.desc()).limit(50)
         if tag is None:
                     return jsonify({"msgs": ["the tag you're looking for could not be found"]}), 404
         result = articles_schema.dump(tag)
@@ -122,18 +122,18 @@ def tags(category):
 def upload():
     toi_rss={#'http://timesofindia.indiatimes.com/rssfeedstopstories.cms': 'Top stories',
              # 'http://timesofindia.indiatimes.com/rssfeeds/1221656.cms': 'Most Recent',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533916/index.rss': 'India',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533917/index.rss': 'World',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533919/index.rss':'Business',
+             'http://timesofindia.feedsportal.com/c/33039/f/533916/index.rss': 'India',
+             'http://timesofindia.feedsportal.com/c/33039/f/533917/index.rss': 'World',
+             'http://timesofindia.feedsportal.com/c/33039/f/533919/index.rss':'Business'
              # 'http://timesofindia.feedsportal.com/c/33039/f/533920/index.rss':'Cricket',
              # 'http://timesofindia.feedsportal.com/c/33039/f/533921/index.rss':'Sports',
              # 'http://dynamic.feedsportal.com/c/33039/f/533968/index.rss':'Health',
              # 'http://timesofindia.feedsportal.com/c/33039/f/533922/index.rss':'Science',
              # 'http://timesofindia.feedsportal.com/c/33039/f/533925/index.rss':'Environment',
-             'http://timesofindia.feedsportal.com/c/33039/f/533923/index.rss':'Technology',
-             'http://timesofindia.feedsportal.com/c/33039/f/533924/index.rss':'Education',
-             'http://timesofindia.feedsportal.com/c/33039/f/533928/index.rss':'Entertainment',
-             'http://timesofindia.indiatimes.com/rssfeeds/2886704.cms':'Lifestyle'
+             # 'http://timesofindia.feedsportal.com/c/33039/f/533923/index.rss':'Technology',
+             # 'http://timesofindia.feedsportal.com/c/33039/f/533924/index.rss':'Education',
+             # 'http://timesofindia.feedsportal.com/c/33039/f/533928/index.rss':'Entertainment',
+             # 'http://timesofindia.indiatimes.com/rssfeeds/2886704.cms':'Lifestyle'
         }
 
 
