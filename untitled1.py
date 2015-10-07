e47b4587bceb673bd52ddc6c71aaee78607c1941
Sanyam <sanyam.jain14@gmail.com>
2015-10-07 17:07:24 +0530

    twitter

--- untitled1.py
+++ untitled1.py
@@ -21,6 +21,7 @@ app.logger.setLevel(logging.ERROR)
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']  #'sqlite:///esoteric.sqlite'  #
 db = SQLAlchemy(app)
+
 json_response = {}
 
 class Article(db.Model):
@@ -121,19 +122,19 @@ def tags(category):
 @app.route("/update-db/", methods=["GET", "POST"])
 def upload():
     toi_rss={'http://timesofindia.indiatimes.com/rssfeedstopstories.cms': 'Top stories',
-              'http://timesofindia.indiatimes.com/rssfeeds/1221656.cms': 'Most Recent'
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533916/index.rss': 'India',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533917/index.rss': 'World',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533919/index.rss':'Business',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533920/index.rss':'Cricket',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533921/index.rss':'Sports',
-             # 'http://dynamic.feedsportal.com/c/33039/f/533968/index.rss':'Health',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533922/index.rss':'Science',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533925/index.rss':'Environment',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533923/index.rss':'Technology',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533924/index.rss':'Education',
-             # 'http://timesofindia.feedsportal.com/c/33039/f/533928/index.rss':'Entertainment',
-             # 'http://timesofindia.indiatimes.com/rssfeeds/2886704.cms':'Lifestyle'
+             'http://timesofindia.indiatimes.com/rssfeeds/1221656.cms': 'Most Recent',
+             'http://timesofindia.feedsportal.com/c/33039/f/533916/index.rss': 'India',
+             'http://timesofindia.feedsportal.com/c/33039/f/533917/index.rss': 'World',
+             'http://timesofindia.feedsportal.com/c/33039/f/533919/index.rss':'Business',
+             'http://timesofindia.feedsportal.com/c/33039/f/533920/index.rss':'Cricket',
+             'http://timesofindia.feedsportal.com/c/33039/f/533921/index.rss':'Sports',
+             'http://dynamic.feedsportal.com/c/33039/f/533968/index.rss':'Health',
+             'http://timesofindia.feedsportal.com/c/33039/f/533922/index.rss':'Science',
+             'http://timesofindia.feedsportal.com/c/33039/f/533925/index.rss':'Environment',
+             'http://timesofindia.feedsportal.com/c/33039/f/533923/index.rss':'Technology',
+             'http://timesofindia.feedsportal.com/c/33039/f/533924/index.rss':'Education',
+             'http://timesofindia.feedsportal.com/c/33039/f/533928/index.rss':'Entertainment',
+             'http://timesofindia.indiatimes.com/rssfeeds/2886704.cms':'Lifestyle'
         }
 
 
@@ -218,7 +219,9 @@ def trend_search(handles):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    json_response.clear()
+    global json_response
+    json_response = {}
+    #json_response.clear()
     for hash in hash_list:
         q = "#"+hash+" -RT"
         alltweets = api.search(q,count=5)
