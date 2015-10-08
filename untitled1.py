bf73fce28b851cef49f7e95b84ba2707c7591580
Sanyam <sanyam.jain14@gmail.com>
2015-10-08 23:48:34 +0530

    chalega

--- untitled1.py
+++ untitled1.py
@@ -46,8 +46,9 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 
-
 class User(db.Model,UserMixin):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(32))
     email=db.Column(db.String(100))
