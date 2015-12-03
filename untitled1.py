ed06604aa8cc8588bcd338893f2fc1c53c470d75
Sanyam <sanyam.jain14@gmail.com>
2015-12-03 13:28:49 +0530

    android fb

--- untitled1.py
+++ untitled1.py
@@ -14,8 +14,9 @@ from flask.ext.bcrypt import generate_password_hash
 # from pip.utils import logging
 # from entity_api import entity_extract
 #from enty import entity
-import entity_api
+#import entity_api
 #from enty import entity
+#import entity_api
 from oauth import OAuthSignIn
 import logging
 
@@ -330,7 +331,7 @@ def oauth_callback(provider):
         user = User(id=social_id, username=username, email=email, general=0)#, friends=friends) #social_id=social_id,
         db.session.add(user)
         db.session.commit()
-        entity_api.entity_extract(social_id, data, 0)
+        #entity_api.entity_extract(social_id, data, 0)
     login_user(user, True)
     return redirect(url_for('personal'))
 
