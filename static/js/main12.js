9e1c77edc714d658f27865b60eabdb8ec3c918d6
Sanyam <sanyam.jain14@gmail.com>
2015-12-04 18:20:00 +0530

    recommended

--- static/js/main12.js
+++ static/js/main12.js
@@ -1,13 +1,25 @@
+$(document).ready(function() {
 
 
-
-
-   $(document).ready(function() {
-
-
-        var value=new Array();
+            var value=new Array();
              var frm=0;
-          var tll=3;
+              var tll=3;
+            var i,j,k;
+
+                 $("#owl-example").owlCarousel({
+
+      autoPlay:3000,
+    //  navigation : true,
+    //  navigationText : ["prev","next"],
+          items:2,
+      itemsDesktop:[980,2],
+      //itemsDesktopSmall : [979,2]
+      itemsDesktopSmall :[950,1],
+
+      itemsTablet:[478,1]
+
+  });
+
 
 
      $.ajax({
@@ -15,7 +27,7 @@
                 url:"/recommended_news",
                 success:function(data, textStatus, xhr) {
                     console.log(data);
-                    var i = 0;
+                     i = 0;
                     for (var prop in data['news'])
                     {
                         value[i++] = data['news'][prop];
@@ -34,7 +46,7 @@
                             $(".content" + k + " " + "img").attr('src',value[j][k-1]['image']);
                             $(".content" + k + " " + ".b1").attr('href','/tagnews/'+value[j][k-1]['category']);
                             $("#a" + k).attr('href','/fullnews/'+value[j][k-1]['id']);
-                        console.log(value[j][k-1]['title'])
+                            console.log(value[j][k-1]['title']);
                     }
                     }
 
@@ -42,21 +54,22 @@
 
 }
 });
-var storeArraynews=new Array();
-var ipp=0;
-            console.log(ipp)
-            type:'GET',
+    var storeArray=new Array();
+        var ipp=0;
+     $.ajax({
+            //console.log(ipp);
+                type:'GET',
                 url:"/recommended_news",
                 success:function(data, textStatus, xhr) {
                     console.log(data);
                     var i = 0;
                     for (var prop in data['news'])
                     {
-                        storeArraynews[i++] = data['news'][prop];
+                        storeArray[i++] = data['news'][prop];
                         //console.log(value[0]);
                     }
-                    console.log(storeArraynews[1][5])
-                     var x = storeArraynews[1][5];
+                    console.log(storeArray[1][5])
+                     var x = storeArray[1][5];
                      var x = x.split("-").join(" ");
                      $("#heading").append("<div>" + x + "</div>");
 
@@ -66,8 +79,8 @@ var ipp=0;
                     var z=0;
                     ipp=ipp-6;
                     console.log(ipp)
-                      for(k=0;k<storeArraynews.length;)
-                       for(j=0;j<storeArraynews[k].length;j++){
+                      for(k=0;k<storeArray.length;k++){
+                       for(j=0;j<storeArray[k].length;j++){
                         //console.log(tlln)
                         $("<div />", { "class":"newsblock", id:"news"+z })
                          .append($("<a />", {  class:"n-a",id:"n-img-a"+z }))
@@ -79,15 +92,16 @@ var ipp=0;
 
                         //$("#n-fullstory" + j).append("<div>" + storeArraynews[j+6][1] + "</div>");
                         $("#n-img-a" + z).append($("<img />", {  class:"n-img",id:"n-img"+z }));
-                        $("#n-title" + z).append(storeArraynews[k][j]['title']);
-                        $("#n-pub" + z).append("<div>" + storeArraynews[k][j]['pubdate'] + "</div>");
-                        $("#n-img" + z).attr('src' , storeArraynews[k][j]['image']);
-                        $("#n-img-a" + z).attr('href','/full_news/'+storeArraynews[k][j]['id']);
-                        $("#n-title" + z).attr('href','/full_news/'+storeArraynews[k][j]['id']);
-               //         $("#n-img-a" + j).prop("href" , "/fullnews/" + storeArraynews[j][4]);
-                        console.log($("kuch aaya" )
-                        var y=storeArraynews[j+6][2];
+                        $("#n-title" + z).append(storeArray[k][j]['title']);
+                        $("#n-pub" + z).append("<div>" + storeArray[k][j]['pubdate'] + "</div>");
+                        $("#n-img" + z).attr('src' , storeArray[k][j]['image']);
+                        $("#n-img-a" + z).attr('href','/full_news/'+storeArray[k][j]['id']);
+                        $("#n-title" + z).attr('href','/full_news/'+storeArray[k][j]['id']);
 
+                           console.log("kuch aaya");
+                           
+                        var y=storeArray[j+6][2];
+                            z++;
 
 
                         if(y=="http://timesofindia.indiatimes.com/photo/7787613.cms"){
@@ -96,28 +110,11 @@ var ipp=0;
                         }
 
 
-                    }
+                    }}
 
-                    loading=false;
-                    $(".loading").remove();
-                },
-                error: function(){
-                    alert("error loading news");
+
                 }
             });
-          $("#owl-example").owlCarousel({
-
-      autoPlay:3000,
-    //  navigation : true,
-    //  navigationText : ["prev","next"],
-          items:2,
-      itemsDesktop:[980,2],
-      //itemsDesktopSmall : [979,2]
-      itemsDesktopSmall :[950,1],
-
-      itemsTablet:[478,1],
-
-  });
 
 });
 
