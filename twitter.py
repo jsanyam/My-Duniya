14cb1f0dbdceb8ee591d66c6de3b176a0d845af1
Sanyam <sanyam.jain14@gmail.com>
2016-03-07 21:15:54 +0530

    First

--- twitter.py
+++ twitter.py
@@ -21,3 +21,28 @@ def get_keywords_twitter(account):
   return keys
 
 #print get_keywords_twitter('ShahRukhKhanFC')
+
+
+# # BING search API
+# @app.route('/<search_type>/<query>')
+# def bing_search(query, search_type = 'Web'):#(query, search_type):
+#     #search_type: Web, Image, News, Video
+#     key= '97VeFEO22dn8nQ9u3zEx7z3QWNlhjcpoRACSnMyaWWg'
+#     query = urllib.quote(query)
+#     # create credential for authentication
+#     #user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
+#     credentials = (':%s' % key).encode('base64')[:-1]
+#     auth = 'Basic %s' % credentials
+#     #Data.ashx
+#     url = 'https://api.datamarket.azure.com/Bing/Search/v1/'+search_type+'?Query=%27'+query+'%27&$top=20&$format=json'
+#     request = urllib2.Request(url)
+#     request.add_header('Authorization', auth)
+#     #request.add_header('User-Agent', user_agent)
+#     request_opener = urllib2.build_opener()
+#     response = request_opener.open(request)
+#     response_data = response.read()
+#     dict_result = json.loads(response_data)
+#     #print(json.dumps(json_result))
+#     result_list = dict_result['d']['results']
+#     return jsonify({'data': result_list})
+#     #return response_data  #not a good view
